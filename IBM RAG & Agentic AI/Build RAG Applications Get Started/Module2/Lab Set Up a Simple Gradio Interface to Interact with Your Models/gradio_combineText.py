9a24737b205b92036a28579d952462b031ea7b58
WWIIITT <ben1207233@gmail.com>
2025-08-07 17:15:25 +0800

    Add Gradio interface lab materials and LLM demos
    Added PDF introduction and multiple Python scripts for Module 2, including Gradio interface examples (combining text, adding numbers, sentence builder) and LLM integration demos using IBM Watsonx and LangChain. These resources support building and interacting with simple Gradio interfaces and demonstrate LLM-based chatbot functionality.

---
+++ IBM RAG & Agentic AI/Build RAG Applications Get Started/Module2/Lab Set Up a Simple Gradio Interface to Interact with Your Models/gradio_combineText.py
@@ -0,0 +1,18 @@
+import warnings
+warnings.filterwarnings('ignore')
+import gradio as gr
+
+def combinetext(a,b):
+    return a + ' ' + b
+
+demo = gr.Interface(
+    fn=combinetext,
+    inputs=[
+        gr.Textbox(label='Input 1'), 
+        gr.Textbox(label='Input 2')
+    ],
+    outputs= gr.Textbox(label='Output')
+
+)
+
+demo.launch(server_name='127.0.0.1', server_port=7860)
---
+++ IBM RAG & Agentic AI/Build RAG Applications Get Started/Module2/Lab Set Up a Simple Gradio Interface to Interact with Your Models/llm_chat.py
@@ -0,0 +1,46 @@
+# Import necessary packages
+from ibm_watsonx_ai.foundation_models import ModelInference
+from ibm_watsonx_ai.metanames import GenTextParamsMetaNames as GenParams
+from ibm_watsonx_ai import Credentials
+from langchain_ibm import WatsonxLLM
+import gradio as gr
+import warnings
+warnings.filterwarnings('ignore')
+
+# Model and project settings
+model_id = 'meta-llama/llama-3-405b-instruct' 
+#model_id = 'ibm/granite-3-3-8b-instruct' # Specify IBM's Granite 3.3 8B model
+
+# Set necessary parameters
+parameters = {
+    GenParams.MAX_NEW_TOKENS: 1024,  # Specify the max tokens you want to generate
+    GenParams.TEMPERATURE: 0.5, # This randomness or creativity of the model's responses
+}
+
+project_id = "skills-network"
+
+# Wrap up the model into WatsonxLLM inference
+watsonx_llm = WatsonxLLM(
+    model_id=model_id,
+    url="https://us-south.ml.cloud.ibm.com",
+    project_id=project_id,
+    params=parameters,
+)
+
+# Function to generate a response from the model
+def generate_response(prompt_txt):
+    generated_response = watsonx_llm.invoke(prompt_txt)
+    return generated_response
+
+# Create Gradio interface
+chat_application = gr.Interface(
+    fn=generate_response,
+    allow_flagging="never",
+    inputs=gr.Textbox(label="Input", lines=2, placeholder="Type your question here..."),
+    outputs=gr.Textbox(label="Output"),
+    title="Watsonx.ai Chatbot",
+    description="Ask any question and the chatbot will try to answer."
+)
+
+# Launch the app
+chat_application.launch(server_name="127.0.0.1", server_port= 7860)
